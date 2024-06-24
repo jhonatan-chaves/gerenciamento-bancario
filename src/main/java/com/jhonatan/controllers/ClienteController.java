4e2788b50bb730dd67488e2a0ddb3cad9c1ee89e
jhonatan-chaves <cjonata799@gmail.com>
2024-06-24 11:37:48 -0300

    metodo get adicionado

--- src/main/java/com/jhonatan/controllers/ClienteController.java
+++ src/main/java/com/jhonatan/controllers/ClienteController.java
@@ -1,11 +1,17 @@
 package com.jhonatan.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 public class ClienteController {
 
     @PostMapping("/")
     public void save(){
+        
+    }
+
+    @GetMapping("/")
+    public void getAll(){
         
     }
 }
