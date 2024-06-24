ca0b129f6380bdb8eeee0061bdb2623bdf198086
jhonatan-chaves <cjonata799@gmail.com>
2024-06-24 03:19:05 -0300

    teste

--- src/main/java/com/jhonatan/controllers/ClienteController.java
+++ src/main/java/com/jhonatan/controllers/ClienteController.java
@@ -1,4 +1,11 @@
 package com.jhonatan.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class ClienteController {
+
+    @PostMapping("/")
+    public void save(){
+        
+    }
 }
